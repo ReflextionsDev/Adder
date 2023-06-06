6b3d2861606ea6f4273db5be1a973307509cdcba
Nathan Bennett <ReflextionsDev@gmail.com>
2023-06-06 10:38:55 -0400

    Demo Init

---
+++ Adder/scripts/importsForEvents.js
@@ -0,0 +1,14 @@
+function getRoomCombinations (size) {
+	if (size === 'xs') {
+		return [[6]]
+	} else if (size === 'sm') {
+		return 10
+	} else if (size === 'md') {
+		return [[14]]
+	}
+}
+
+function add(a, b)
+{
+	return a + b;
+}
